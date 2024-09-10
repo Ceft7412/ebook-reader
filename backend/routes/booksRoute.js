00e5c51de92d8e99b88a80c537ad3bd6412e7e69
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-10 14:07:28 +0800

    @update Delete the backend directory as this web app is primarily client interaction.

--- backend/routes/booksRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { books } from "../controllers/booksController.js";
-
-router.get("/", books);
-
-export default router;
--- frontend/src/utils/slugify.ts
+++
@@ -1,9 +0,0 @@
-export function slugify(text: string) {
-  return text
-    .toString() // Cast to string
-    .toLowerCase() // Convert the string to lowercase
-    .trim() // Remove whitespace from both sides of a string
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
-}
